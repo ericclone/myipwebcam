28f626bcdc4f9857214cd3749e37a7e1276a7548
Eric <eric.clone@gmail.com>
2017-06-22 11:33:36 -0700

    Load ip from github repo

---
+++ getsource.js
@@ -0,0 +1,21 @@
+var url = "https://raw.githubusercontent.com/ericclone/myipwebcam/master/sources.json"
+
+var ajax = new XMLHttpRequest();
+ajax.open("GET", url, true);
+ajax.send(null);
+ajax.onreadystatechange = function () {
+
+     if (ajax.readyState == 4 && (ajax.status == 200)) {
+
+        console.log("ready")            
+        var dict = JSON.parse(ajax.responseText);
+        var top = dict.top;
+        var bottom = dict.bottom;
+        document.getElementById("iftop").src = "http://" + top + ":8080/browserfs.html";
+        document.getElementById("ifbottom").src = "http://" + bottom + ":8080/browserfs.html";
+        document.getElementById("btntop").href = "http://" + top + ":8080";
+        document.getElementById("btnbottom").href = "http://" + bottom + ":8080";
+    } else {
+        console.log("not ready yet")            
+    }
+}
