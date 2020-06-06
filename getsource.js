a5e1192ef4fa3b81cb56ee564c6b7cde1ee84392
ericclone <eric.clone@gmail.com>
2020-06-06 16:52:52 -0700

    remove unused iframe

--- getsource.js
+++ getsource.js
@@ -9,11 +9,8 @@ ajax.onreadystatechange = function () {
 
         console.log("ready")            
         var dict = JSON.parse(ajax.responseText);
-//         var top = dict.top;
         var bottom = dict.bottom;
-//         document.getElementById("iftop").src = "http://" + top + ":8080/browserfs.html";
         document.getElementById("ifbottom").src = "http://" + bottom + ":8080/browserfs.html";
-//         document.getElementById("btntop").href = "http://" + top + ":8080";
         document.getElementById("btnbottom").href = "http://" + bottom + ":8080";
     } else {
         console.log("not ready yet")            
